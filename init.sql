6616e854d32575b162adda998d9ab6411bd12415
Ubuntu <ubuntu@ip-172-31-34-34.us-east-2.compute.internal>
2020-04-09 07:14:52 +0000

    registry implementation

--- init.sql
+++ init.sql
@@ -1,10 +1,8 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    first_name varchar(10),
-    last_name varchar(15),
-    email varcar(15),
-    username varchar(15),
-    password varchar(15)
+    name TEXT,
+    email TEXT,
+    password TEXT,
 );
     
 CREATE TABLE todos (
@@ -12,11 +10,11 @@ CREATE TABLE todos (
     title varchar(20),
     description varchar(50),
     due_date DATE,
-    user_id integer
-    is_complete
+    is_complete boolean,
+    user_id integer,
 );
 
-INSERT INTO users(username, password) VALUES ('peter', 'marcylabschool');
+INSERT INTO users(name email, password) VALUES ('peter', 'peter@bk.com', 'marcylab');
 
 INSERT INTO todos(title, description, dueDate, user_id) VALUES ('Clean Your Room.', 'Vacuum, the floor & shit',TO_DATE('07/04/2020', 'DD/MM/YYYY'), 1);
 INSERT INTO todos(title, description, dueDate, user_id) VALUES ('Do Homework.', 'Complete Problem Set & shit',TO_DATE('07/05/2020', 'DD/MM/YYYY'), 1);
